66bea2e07a8615cb03a451ed1cf613e11a5056ce
Alex Nicolas <alexoliveira604@gmail.com>
2023-04-02 14:30:14 -0300

    feat: POST /list endpoint should return int sent from body by client side

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,13 @@
 package com.example.demo;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +29,12 @@ public class DemoApplication {
     public ResponseEntity<int[]> list() {
 	  int array[] = { 10, 20, 30, 40, 50 };
       return ResponseEntity.ok(array);
+    }
+
+    @PostMapping("/list")
+    public ResponseEntity<List<Integer>> listAddItem(@RequestBody int newInt) {
+	  List<Integer> list = new ArrayList<Integer>();
+	  list.add(newInt);
+      return ResponseEntity.ok(list);
     }
 }
