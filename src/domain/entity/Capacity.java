2a0ab55de83d7dfb5a8f6ad85971aed1351b9319
Alex Nicolas <alexoliveira604@gmail.com>
2023-04-02 13:25:06 -0300

    feat: spring boot start project

---
+++ src/domain/entity/Capacity.java
@@ -0,0 +1,11 @@
+package com.clean.example.core.entity;
+
+public class Capacity {
+    private boolean storage;
+    private boolean test;
+
+    public Capacity(boolean storage, boolean test) {
+        this.test = test;
+        this.storage = storage;
+    }
+}
