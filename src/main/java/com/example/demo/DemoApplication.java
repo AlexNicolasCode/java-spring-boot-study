7d22d34e42ce20ed1d4f3dd41cb23c83c00df587
Alex Nicolas <alexoliveira604@gmail.com>
2023-04-02 13:45:48 -0300

    feat: GET /list should return integrer array in return

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +18,11 @@ public class DemoApplication {
 		  return String.format("test is running!");
 	  }
       return String.format("Hello %s!", name);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<int[]> list() {
+	  int array[] = { 10, 20, 30, 40, 50 };
+      return ResponseEntity.ok(array);
     }
 }
